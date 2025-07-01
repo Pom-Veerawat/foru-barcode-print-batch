e0f26cdb837b5eb21dd9f903c1686ab08b3e3ca2
pom <118708779+Pom-Veerawat@users.noreply.github.com>
2025-07-02 00:04:22 +0700

    up

---
+++ src/components/BarcodeData copy.js
@@ -0,0 +1,63 @@
+import React from "react";
+import classes from "./BarcodeData.module.css";
+import Barcode from "react-barcode";
+
+const BarcodeData = (props) => {
+  console.log("props BarcodeData", props);
+  const desAll = () => {
+    let val = props.des;
+    if (val.length < 15) {
+      const addMroe = 15 - val.length;
+      //console.log(addMroe);
+      for (let index = 0; index < addMroe; index++) {
+        val = val + "--";
+      }
+    }
+    //console.log(val);
+    //return "000000000000000"
+    //return props.des + " ราคาปกติ " + props.gp19 + " บาท";
+    return "      ราคาปกติ " + props.gp19 + " บาท";
+  };
+  const number = parseFloat(props.gp20).toFixed(2);
+  const [intPart, decimalPart] = number.split(".");
+  //desAll();
+  return (
+    <div
+      className={classes.allinline}
+      onClick={props.onDeleteHandler.bind(this, props.itemId)}
+    >
+      <div className={classes.maindatabarcode}>
+        <div className={classes.title}>{props.title} </div>
+
+        <div>
+          <div className={classes.bath}> บาท</div>
+          <div className={classes.price}>
+            <span className={classes.intPart}>{intPart}</span>
+            <span className={classes.decimalPart}>.{decimalPart}</span>
+          </div>
+          <div style={{ flexDirection: "column" }}>
+            <div className={classes.title2}>ราคาสมาชิก</div>
+            <div className={classes.unit}> {props.unit}</div>
+          </div>
+        </div>
+        <div className={classes.position}>{desAll()}</div>
+      </div>
+      <div className={classes.seconddatabarcode}>
+        {/* <div>{props.barcode}</div> */}
+        <div style={{ marginTop: "-10px" }}>
+          <Barcode
+            style={{ marginTop: "-10px" }}
+            value={props.barcode}
+            width={1.2}
+            height={20}
+            flat={true}
+            fontSize={props.fontV}
+            displayValue={false}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BarcodeData;
--- src/components/BarcodeData.js
+++ src/components/BarcodeData.js
@@ -3,50 +3,56 @@ import classes from "./BarcodeData.module.css";
 import Barcode from "react-barcode";
 
 const BarcodeData = (props) => {
-  console.log("props BarcodeData", props);
+  // --- Data Handling Fixes ---
+  // 1. Provide a default value for the "regular price" if props.gp19 is missing.
   const desAll = () => {
-    let val = props.des;
-    if (val.length < 15) {
-      const addMroe = 15 - val.length;
-      //console.log(addMroe);
-      for (let index = 0; index < addMroe; index++) {
-        val = val + "--";
-      }
-    }
-    //console.log(val);
-    //return "000000000000000"
-    //return props.des + " ราคาปกติ " + props.gp19 + " บาท";
-    return "      ราคาปกติ " + props.gp19 + " บาท";
+    const priceText = props.gp19 || "0.00";
+    return "ราคาปกติ " + priceText + " บาท";
   };
-  const number = parseFloat(props.gp20).toFixed(2);
+
+  // 2. Provide a fallback of 0 if props.gp20 (member price) is missing to prevent NaN.
+  const number = parseFloat(props.gp20 || 0).toFixed(2);
   const [intPart, decimalPart] = number.split(".");
-  //desAll();
+
   return (
     <div
       className={classes.allinline}
-      onClick={props.onDeleteHandler.bind(this, props.itemId)}
+      onClick={
+        props.onDeleteHandler
+          ? props.onDeleteHandler.bind(this, props.itemId)
+          : null
+      }
     >
       <div className={classes.maindatabarcode}>
-        <div className={classes.title}>{props.title} </div>
-      
-        <div>
-          <div className={classes.bath}> บาท</div>
-          <div className={classes.price}>
-            <span className={classes.intPart}>{intPart}</span>
-            <span className={classes.decimalPart}>.{decimalPart}</span>
+        <div className={classes.title}>{props.title}</div>
+
+        {/* Flexbox container for price details */}
+        <div className={classes.detailsContainer}>
+          {/* Left side content */}
+          <div className={classes.leftDetails}>
+            <div className={classes.title2}>ราคาสมาชิก</div>
+            <div className={classes.unit}>{props.unit}</div>
           </div>
-          <div style={{ flexDirection: "column" }}>
-              <div className={classes.title2}>ราคาสมาชิก</div>
-            <div className={classes.unit}> {props.unit}</div>
+
+          {/* Right side content */}
+          <div className={classes.rightDetails}>
+            <div className={classes.price}>
+              <span className={classes.intPart}>{intPart}</span>
+              <span className={classes.decimalPart}>.{decimalPart}</span>
+            </div>
+            <div className={classes.bath}>บาท</div>
           </div>
         </div>
+
+        {/* Regular price line */}
         <div className={classes.position}>{desAll()}</div>
       </div>
+
+      {/* Barcode container */}
       <div className={classes.seconddatabarcode}>
-        {/* <div>{props.barcode}</div> */}
-        <div>
+        <div className={classes.barcodeWrapper}>
           <Barcode
-            value={props.barcode}
+            value={props.barcode || "00000000"} // Add fallback for barcode
             width={1.2}
             height={17}
             flat={true}
